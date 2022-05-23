1a9b5aab56cd14a2f2897b9b4b9e6a0b9c83f3c5
FG98 <81941690+FG98F@users.noreply.github.com>
2022-05-22 23:05:24 -0400

    Create fgwab.js

---
+++ FGWA/fgwab.js
@@ -0,0 +1,10 @@
+{
+  "records": [
+    {
+      "version": "FGWA Business v0.0.1",
+      "code": 1,
+      "url": "https://fgwhatsapp.ml",
+      "changelog": "- Primer lanzamiento"
+    }
+  ]
+}
