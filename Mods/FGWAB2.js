1a723d1284d90645fa34d4f3d9622adabea4bf06
FG98 <81941690+FG98F@users.noreply.github.com>
2022-06-14 21:21:23 -0400

    Create FGWAB2.js

---
+++ Mods/FGWAB2.js
@@ -0,0 +1,14 @@
+{
+	"version": "14.0",
+	"download": "https://fgmods.epizy.com/fgwhatsapp/com.fgw4b.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://"
+		}
+	]
+}
